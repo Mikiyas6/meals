e7977fb2d2d923db1583a6a718e98992188f458f
Mikiyas6 <Mikitedi9@gmail.com>
2026-07-13 17:05:25 +0200

    Add device preview support and implement categories screen layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/categories.dart';
 
 /*
   In Flutter, ThemeData is the object that holds the visual styling for your app, such as colors, fonts, and overall design rules.
@@ -24,7 +27,9 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const App()),
+  );
 }
 
 class App extends StatelessWidget {
@@ -33,8 +38,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: theme,
-      home: // Todo ...,
+      home: CategoriesScreen(),
     );
   }
 }
