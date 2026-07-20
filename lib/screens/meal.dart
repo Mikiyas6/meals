90e6d458e0f2ef85ae7d6b9cedf4a39c30ca8643
Mikiyas6 <Mikitedi9@gmail.com>
2026-07-20 16:20:22 +0200

    Refactor category grid item and meal item components for improved structure and functionality

--- lib/screens/meal.dart
+++ lib/screens/meal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/data/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 // import 'package:meals/data/dummy_data.dart';
 
 class MealScreen extends StatelessWidget {
@@ -12,120 +13,9 @@ class MealScreen extends StatelessWidget {
     List<Meal> filteredMeals = dummyMeals
         .where((meal) => meal.categories.contains(id))
         .toList();
-    Widget mainContent = filteredMeals.isEmpty
-        ? Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "Oh oh.... Nothing Here!",
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                ),
-                Text(
-                  "Try selecting a different Category.",
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                ),
-              ],
-            ),
-          )
-        : ListView.builder(
-            itemCount: filteredMeals.length,
-            itemBuilder: (context, index) => Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-              height: 195,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(filteredMeals[index].imageUrl),
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(200, 0, 0, 0),
-                    ),
-                    padding: EdgeInsets.all(6),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      spacing: 2,
-                      children: [
-                        Text(
-                          filteredMeals[index].title,
-                          style: Theme.of(context).textTheme.titleMedium
-                              ?.copyWith(
-                                color: Theme.of(context).colorScheme.onSurface,
-                              ),
-                        ),
-                        SizedBox(height: 5),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              spacing: 5,
-                              children: [
-                                Icon(Icons.timelapse_rounded),
-                                Text(
-                                  '${filteredMeals[index].duration} min',
-                                  style: Theme.of(context).textTheme.bodyMedium!
-                                      .copyWith(
-                                        color: Theme.of(
-                                          context,
-                                        ).colorScheme.onSurface,
-                                      ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              spacing: 5,
-                              children: [
-                                Icon(Icons.shopping_bag),
-                                Text(
-                                  filteredMeals[index].complexity.name,
-                                  style: Theme.of(context).textTheme.bodyLarge!
-                                      .copyWith(
-                                        color: Theme.of(
-                                          context,
-                                        ).colorScheme.onSurface,
-                                      ),
-                                ),
-                              ],
-                            ),
-
-                            Row(
-                              spacing: 5,
-                              children: [
-                                Icon(Icons.attach_money),
-                                Text(
-                                  filteredMeals[index].affordability.name,
-                                  style: Theme.of(context).textTheme.bodyLarge!
-                                      .copyWith(
-                                        color: Theme.of(
-                                          context,
-                                        ).colorScheme.onSurface,
-                                      ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
     return Scaffold(
       appBar: AppBar(title: Text(title), centerTitle: false),
-      body: mainContent,
+      body: MealItem(meals: filteredMeals),
     );
   }
 }
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meals});
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = meals.isEmpty
+        ? Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Oh oh.... Nothing Here!",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+                Text(
+                  "Try selecting a different Category.",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: meals.length,
+            itemBuilder: (context, index) => InkWell(
+              child: Card(
+                elevation: 2,
+                margin: EdgeInsets.all(8),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(8),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Stack(
+                  children: [
+                    FadeInImage(
+                      placeholder: MemoryImage(kTransparentImage),
+                      image: NetworkImage(meals[index].imageUrl),
+                      // fit: BoxFit.cover,
+                      // height: 210,
+                      // width: double.infinity,
+                    ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 6,
+                          horizontal: 44,
+                        ),
+                        color: Colors.black54,
+                        child: Column(
+                          children: [
+                            Text(
+                              meals[index].title,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                              softWrap: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+    return mainContent;
+  }
+}
+
+
+// Container(
+//               margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+//               height: 195,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(10),
+//                 image: DecorationImage(
+//                   fit: BoxFit.cover,
+//                   image: NetworkImage(meals[index].imageUrl),
+//                 ),
+//               ),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.end,
+//                 children: [
+//                   Container(
+//                     decoration: BoxDecoration(
+//                       color: Color.fromARGB(200, 0, 0, 0),
+//                     ),
+//                     padding: EdgeInsets.all(6),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       spacing: 2,
+//                       children: [
+//                         Text(
+//                           meals[index].title,
+//                           style: Theme.of(context).textTheme.titleMedium
+//                               ?.copyWith(
+//                                 color: Theme.of(context).colorScheme.onSurface,
+//                               ),
+//                         ),
+//                         SizedBox(height: 5),
+//                         Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                           crossAxisAlignment: CrossAxisAlignment.center,
+//                           children: [
+//                             Row(
+//                               spacing: 5,
+//                               children: [
+//                                 Icon(Icons.timelapse_rounded),
+//                                 Text(
+//                                   '${meals[index].duration} min',
+//                                   style: Theme.of(context).textTheme.bodyMedium!
+//                                       .copyWith(
+//                                         color: Theme.of(
+//                                           context,
+//                                         ).colorScheme.onSurface,
+//                                       ),
+//                                 ),
+//                               ],
+//                             ),
+//                             Row(
+//                               spacing: 5,
+//                               children: [
+//                                 Icon(Icons.shopping_bag),
+//                                 Text(
+//                                   meals[index].complexity.name,
+//                                   style: Theme.of(context).textTheme.bodyLarge!
+//                                       .copyWith(
+//                                         color: Theme.of(
+//                                           context,
+//                                         ).colorScheme.onSurface,
+//                                       ),
+//                                 ),
+//                               ],
+//                             ),
+
+//                             Row(
+//                               spacing: 5,
+//                               children: [
+//                                 Icon(Icons.attach_money),
+//                                 Text(
+//                                   meals[index].affordability.name,
+//                                   style: Theme.of(context).textTheme.bodyLarge!
+//                                       .copyWith(
+//                                         color: Theme.of(
+//                                           context,
+//                                         ).colorScheme.onSurface,
+//                                       ),
+//                                 ),
+//                               ],
+//                             ),
+//                           ],
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
